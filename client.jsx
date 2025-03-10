7f0d5a61e2c3e49910e3facd0cad6edea66180bc
hyorish03 <hyorish2001@gmail.com>
2025-03-10 22:17:50 +0900

    [Webpack] 기본 번들링 설정

---
+++ client.jsx
@@ -0,0 +1,5 @@
+const React = require("react");
+const ReactDom = require("react-dom");
+const { NoCRA } = require("./NoCRA");
+
+ReactDom.render(<NoCRA />, document.querySelector("root"));
---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require("path");
+
+module.exports = {
+  name: "NoCRA",
+  mode: "development",
+  devtool: "eval",
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  entry: {
+    app: ["./client"],
+  }, //입력
+  output: {
+    path: path.join(__dirname, "dist"), // 실제 로컬 경로는 user/hyorinlee/github/.../dist 이겠지만, 현재 파일이 위치한 폴더(__dirname)를 기준으로 "dist" 폴더의 절대 경로를 생성
+    filename: "app.js",
+  }, //출력
+};
