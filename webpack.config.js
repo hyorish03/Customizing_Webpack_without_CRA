bc7be7c0420191a586efa2d36aa8e2f99bdb88e9
hyorish03 <hyorish2001@gmail.com>
2025-03-11 13:46:18 +0900

    [Webpack] webpack-dev-server 설정 추가

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const path = require("path");
-
 module.exports = {
   name: "NoCRA",
   mode: "development",
@@ -25,4 +24,9 @@ module.exports = {
     path: path.join(__dirname, "dist"), // 실제 로컬 경로는 user/hyorinlee/github/.../dist 이겠지만, 현재 파일이 위치한 폴더(__dirname)를 기준으로 "dist" 폴더의 절대 경로를 생성
     filename: "app.js",
   }, //출력
+  devServer: {
+    static: path.join(__dirname, "dist"), // 빌드된 파일들이 위치할 폴더
+    port: 3000, // 서버가 실행될 포트 번호
+    open: true, // 서버 시작 시 자동으로 브라우저 열기
+  },
 };
